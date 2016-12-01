a1c78d3a39886123b62e66ba64ec9fa221dcd52c
Yohan Mishkin <yohanmishkin@protonmail.com>
2016-12-01 02:21:48 -0500

    Work in progress
    See: https://github.com/ember-cli/ember-cli-todos/blob/master/tests/acceptance/todos-test.js

--- tests/acceptance/disasters-test.js
+++ tests/acceptance/disasters-test.js
@@ -6,10 +6,23 @@ moduleForAcceptance('Acceptance | disasters');
 test('visiting /disasters, no disasters', function(assert) {
   visit('/disasters');
 
-  andThen(function() {
+  andThen(() => {
     assert.equal(currentURL(), '/disasters');
     assert.equal(find('.test-disasters-header').length, 1, 'header appears');
     assert.equal(find('.test-disasters-create-new').length, 1, 'create new link is visible');
     assert.equal(find('.test-disasters-table > tr').length, 0, 'no disasters visible');
   });
+});
+
+test('can create new disaster', function(assert) {
+	visit('/disasters');
+
+	click('.test-disasters-create-new');
+	fillIn('.test-disaster-name-input', 'Hurricane Dandy');
+	click('.test-disasters-create-new-submit');
+
+	andThen(() => {
+		assert.equal(find('.test-disasters-table > tr').length, 1, 'disaster added to the table');
+		assert.equal(find('.test-disasters-table > tr:first').text(), 'Hurricane Dandy', 'disaster name seen in table');
+	});
 });
