73e86ba39b3c32e7430cf33d883221e6c7bd7f58
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-01-31 22:18:02 -0500

    Added worksite name & locaiton

--- tests/acceptance/disasters/edit/worksites-test.js
+++ tests/acceptance/disasters/edit/worksites-test.js
@@ -6,13 +6,15 @@ moduleForAcceptance('Acceptance | disasters/edit/worksites');
 
 test('Can navigate to worksite page', function(assert) {
   let disaster = server.create('disaster', { name: 'Hurricane Daniel', slug: 'hurricane-daniel' });
-  server.create('workSite', { name: 'Ticonderoga', disaster });
+  server.create('workSite', { name: 'Ticonderoga', location: '12 Candy Lane', disaster });
 
   page
     .visit({disaster_slug: disaster.slug})
     .worksite();
   
   andThen(function() {
-    assert.equal(currentURL(), '/disasters/hurricane-daniel/sites/ticonderoga');
+    assert.equal(currentURL(), '/disasters/hurricane-daniel/sites/ticonderoga', 'Navigated to worksite page');
+    assert.equal(find('.test-work-site-name').text(), 'Ticonderoga', 'Worksite name displayed');
+    assert.equal(find('.test-work-site-location').text(), '12 Candy Lane', 'Worksite location displayed');
   });
 });
