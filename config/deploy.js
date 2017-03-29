4c893ede6c85c809f3764701b2d5a06af93512f6
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-03-28 22:08:56 -0400

    Setting up deploy to Pagefront
    #15

--- config/deploy.js
+++ config/deploy.js
@@ -1,32 +1,8 @@
-/* jshint node: true */
-
-module.exports = function(deployTarget) {
-  var ENV = {
-    build: {},
-    // include other plugin configuration that applies to all deploy targets here
+module.exports = function(deployTarget) {  
+  return {
     pagefront: {
-      app: 'launchdock',
+      app: 'crosscheck',
       key: process.env.PAGEFRONT_KEY
     }
   };
-
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
-
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
-
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-  }
-
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
-  return ENV;
 };
