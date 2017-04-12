6d9626c7150836c6864b13c236bc0a36fcc5a1da
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-04-12 00:20:07 -0400

    Moved calling and checking in to actions on roster-row

--- tests/integration/components/roster-row-test.js
+++ tests/integration/components/roster-row-test.js
@@ -17,4 +17,20 @@ test('Not checked in volunteer renders', function(assert) {
   this.render(hbs`{{roster-row volunteer=volunteer}}`);
 
   assert.equal(this.$('.test-volunteer-time-checked-in').text().trim(), '-', 'Dash displayed for checkin time when not checked in');
+  assert.ok(this.$('.test-checkin-link'), 'Checkin link appears');
+});
+
+test('Checked-in volunteer renders', function(assert) {
+
+  let volunteer = Ember.Object.create({
+    name: 'Charlie Sines',
+    timeCheckedIn: new Date('2012-12-12 12:00'),
+    isCheckedIn: true
+  });
+  this.set('volunteer', volunteer);
+  
+  this.render(hbs`{{roster-row volunteer=volunteer}}`);
+
+  assert.equal(this.$('.test-volunteer-time-checked-in').text().trim(), '12:00 PM', 'Checkin time displays');
+  assert.equal(this.$('.test-checkout-link').length, 1, 'Checkout link appears');
 });
