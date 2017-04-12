3f35b5fe4efeed97b28ad0ed03227b835c45a4bc
Tommy Jebb <tjebb@steadfast.com>
2017-04-12 16:28:45 -0400

    Added redirect to login & added favicon

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,11 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     session: Ember.inject.service(),
     beforeModel() {
-        return this.get('session').fetch().catch(() => {});
+        return this.get('session')
+            .fetch()
+            .catch((e) => {
+                console.log(e);
+                this.transitionTo('login');
+            });
     },
     actions: {
         logOut() {
-            this.get('session').close();
+            this.get('session')
+                .close()
+                .then(() => {
+                    this.transitionTo('login');
+                });
         }
     }
 });
