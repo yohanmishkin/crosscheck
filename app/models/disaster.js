d70ca180afbcf8a2bd6a5a622241125bb327c936
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-04-13 00:34:20 -0400

    Added operation total counts
    Closes #27

--- app/models/disaster.js
+++ app/models/disaster.js
@@ -4,5 +4,17 @@ import Ember from 'ember';
 export default DS.Model.extend({
 	name: DS.attr('string'),
 	sites: DS.hasMany('site'),
-	slug: Ember.computed('name', function() { return this.get('name').dasherize(); })
+	slug: Ember.computed('name', function() { return this.get('name').dasherize(); }),
+	
+	volunteersNotCheckedInArray: Ember.computed.mapBy('sites', 'notCheckInCount'),
+	volunteersCheckedInArray: Ember.computed.mapBy('sites', 'checkInCount'),
+	volunteersCheckedOutArray: Ember.computed.mapBy('sites', 'checkOutCount'),
+
+	volunteersNotCheckedIn: Ember.computed.sum('volunteersNotCheckedInArray'),
+	volunteersCheckedIn: Ember.computed.sum('volunteersCheckedInArray'),
+	volunteersCheckedOut: Ember.computed.sum('volunteersCheckedOutArray'),
+
+	hasNotCheckIns: Ember.computed.gt('volunteersNotCheckedIn', 0),
+	hasCheckIns: Ember.computed.gt('volunteersCheckedIn', 0),
+	hasCheckOuts: Ember.computed.gt('volunteersCheckedOut', 0)
 });
